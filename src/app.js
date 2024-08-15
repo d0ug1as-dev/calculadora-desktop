9c2ee4426c5e302c950d1baeb47516755d02380b
Douglas J. Carvalho <dev-douglas@outlook.com.br>
2024-08-15 18:54:27 -0300

    btns de delete - btn dell (arrow) limpa um por vez, btn dell (C) limpa toda operacao

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@
 let num1 = [];
 let num2 = [];
 let operador;
+let arrayResult = [];
 
 /* pegando valor dos numeros e jogando no visor e no operador acima */
 function getValue(varNum, varTag){
@@ -10,6 +11,7 @@ function getValue(varNum, varTag){
     getNum = varNum
     return getNum
 }
+/* pegando valor dos numeros e jogando no visor e no operador acima */
 getValue('zero', '.zero').addEventListener('click', validaNum)
 getValue('um', '.um').addEventListener('click', validaNum)
 getValue('dois', '.dois').addEventListener('click', validaNum)
@@ -21,13 +23,6 @@ getValue('sete', '.sete').addEventListener('click', validaNum)
 getValue('oito', '.oito').addEventListener('click', validaNum)
 getValue('nove', '.nove').addEventListener('click', validaNum)
 getValue('ponto', '.ponto').addEventListener('click', validaNum)
-/* pegando valor dos operadores e jogando no visor e no operador acima */
-getValue('divisao', '.divisao').addEventListener('click', validaOperador)
-getValue('multiplicacao', '.multiplicacao').addEventListener('click', validaOperador)
-getValue('subtracao', '.subtracao').addEventListener('click', validaOperador)
-getValue('adicao', '.adicao').addEventListener('click', validaOperador)
-
-
 function validaNum(){
     if(operador == '' || operador == undefined){
         num1.push(this.value)
@@ -44,9 +39,45 @@ function validaNum(){
         getValue('operacao', '.operacao').innerText =  arrayResult.join('')
     }
 }
+/* pegando valor dos operadores e jogando no visor e no operador acima */
+getValue('divisao', '.divisao').addEventListener('click', validaOperador)
+getValue('multiplicacao', '.multiplicacao').addEventListener('click', validaOperador)
+getValue('subtracao', '.subtracao').addEventListener('click', validaOperador)
+getValue('adicao', '.adicao').addEventListener('click', validaOperador)
 function validaOperador(){
     operador = this.value
     arrayResult.push(operador)
     getValue('visor', '.visor').value  += operador
     getValue('operacao', '.operacao').innerText +=  operador
+}
+/* LIMPANDO PARA UMA NOVA OPERAÇÃO */
+function limpaStage(){
+    num1.splice(0, arrayResult.length)
+    num2.splice(0, arrayResult.length)
+    operador = ''
+  getValue('visor', '.visor').value  = ''
+   arrayResult.splice(0, arrayResult.length)
+}
+/* botao delete */
+getValue('cReset', '.cReset').addEventListener('click', btnCReset)
+function btnCReset(){
+    limpaStage()
+    getValue('visor', '.visor').value = ""
+    getValue('operacao', '.operacao').innerText = ""
+}
+/* botao delete */
+getValue('dell', '.dell').addEventListener('click',  btnDell)
+function btnDell(){
+    if(operador == '' || operador == undefined){
+        num1.pop()
+    }
+    if(num2 == ""){
+        operador = ''
+    }
+    if(num1 !== "" && num1 !== undefined && operador !== ""  && operador !== undefined){
+    num2.pop() 
+}
+    arrayResult.pop()
+    getValue('visor', '.visor').value  = arrayResult.join('')
+        getValue('operacao', '.operacao').innerText = arrayResult.join('')
 }
